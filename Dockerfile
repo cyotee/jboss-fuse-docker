9913c0ebf00d7b5fb95dd0d612f667ba22fb1be3
cyotee <cyotee@gmail.com>
2016-02-07 11:10:58 -0800

    Attempt to address permissions problems during install

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,13 @@ FROM jboss/base-jdk:7
 MAINTAINER Robert Greathouse <robert.i.greathouse@gmail.com>
 
 # Set the FUSE_VERSION env variable
-ENV FUSE_VERSION 6.2.0.redhat-099
-ENV FUSE_ARTIFACT_ID=jboss-fuse-karaf-full
-ENV FUSE_DISTRO_URL=http://origin-repository.jboss.org/nexus/content/groups/ea/org/jboss/fuse/${FUSE_ARTIFACT_ID}/${FUSE_VERSION}/${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
+#ENV FUSE_VERSION 6.2.0.redhat-099
+#ENV FUSE_ARTIFACT_ID=jboss-fuse-karaf-full
+ENV FUSE_ARTIFACT_ID=fuse.zip
+#ENV FUSE_DISTRO_URL=http://origin-repository.jboss.org/nexus/content/groups/ea/org/jboss/fuse/${FUSE_ARTIFACT_ID}/${FUSE_VERSION}/${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
 ENV JBOSS_DIR=/opt/jboss
-ENV FUSE_DIR=$JBOSS_DIR/jboss-fuse
-ENV FUSE_HOME=$FUSE_DIR/bin
+ENV FUSE_HOME=$JBOSS_DIR/fuse
+
 # If the container is launched with re-mapped ports, these ENV vars should
 # be set to the remapped values.
 ENV FUSE_PUBLIC_OPENWIRE_PORT 61616
@@ -22,37 +23,10 @@ ENV FUSE_PUBLIC_AMQP_SSL_PORT 5671
 ENV FUSE_PUBLIC_STOMP_SSL_PORT 61614
 
 # Install fuse in the image.
-#
-RUN mkdir $JBOSS_DIR
-RUN mkdir $FUSE_DIR
-
-RUN curl -O  ${FUSE_DISTRO_URL} && \
-    jar -xvf ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip -C $JBOSS_DIR/jboss-fuse && \
-    rm ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip && \
-    chmod a+x $JBOSS_DIR/jboss-fuse/bin/* && \
-    rm $JBOSS_DIR/jboss-fuse/bin/*.bat && \
-    rm $JBOSS_DIR/jboss-fuse/bin/start && \
-    rm $JBOSS_DIR/jboss-fuse/bin/stop && \
-    rm $JBOSS_DIR/jboss-fuse/bin/status && \
-    rm $JBOSS_DIR/jboss-fuse/bin/patch && \
-    rm -rf $JBOSS_DIR/jboss-fuse/extras && \
-    rm -rf $JBOSS_DIR/jboss-fuse/quickstarts && \
-    sed -i -e 's/environment.prefix=FABRIC8_/environment.prefix=FUSE_/' $JBOSS_DIR/jboss-fuse/etc/system.properties && \
-    sed -i -e '/karaf.name = root/d' $JBOSS_DIR/jboss-fuse/etc/system.properties && \
-    sed -i -e '/runtime.id=/d' $JBOSS_DIR/jboss-fuse/etc/system.properties && \
-    mv $JBOSS_DIR/jboss-fuse/data/tmp $JBOSS_DIR/jboss-fuse/tmp && \
-    echo 'org.osgi.framework.storage=${karaf.base}/tmp/cache'>> $JBOSS_DIR/jboss-fuse/etc/config.properties && \
-    sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' $JBOSS_DIR/jboss-fuse/bin/karaf && \
-    sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' $JBOSS_DIR/jboss-fuse/bin/fuse && \
-    sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' $JBOSS_DIR/jboss-fuse/bin/client && \
-    sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' $JBOSS_DIR/jboss-fuse/bin/admin && \
-    sed -i -e 's/${karaf.data}\/generated-bundles/${karaf.base}\/tmp\/generated-bundles/' $JBOSS_DIR/jboss-fuse/etc/org.apache.felix.fileinstall-deploy.cfg && \
-    sed -i -e 's/karaf.delay.console=true/karaf.delay.console=false/' $JBOSS_DIR/jboss-fuse/etc/config.properties;
-
-
-COPY users.properties $JBOSS_DIR/jboss-fuse/etc/users.properties
-COPY install.sh $JBOSS_DIR/install.sh
-RUN $JBOSS_DIR/install.sh
+COPY $FUSE_ARTIFACT_ID $JBOSS_DIR
+RUN jar -xvf $JBOSS_DIR/${FUSE_ARTIFACT_ID} && \
+    rm /$JBOSS_DIR/$FUSE_ARTIFACT_ID
+COPY fuse/ $FUSE_HOME
 
 EXPOSE 8181 8101 1099 44444 61616 1883 5672 61613 61617 8883 5671 61614
 
--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 #
 
 # Adjust the following env vars if needed.
-#FUSE_ARTIFACT_ID=jboss-fuse-karaf-full
-#FUSE_DISTRO_URL=http://origin-repository.jboss.org/nexus/content/groups/ea/org/jboss/fuse/${FUSE_ARTIFACT_ID}/${FUSE_VERSION}/${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
+FUSE_ARTIFACT_ID=jboss-fuse-karaf-full
+FUSE_DISTRO_URL=http://origin-repository.jboss.org/nexus/content/groups/ea/org/jboss/fuse/${FUSE_ARTIFACT_ID}/${FUSE_VERSION}/${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
 
 # Lets fail fast if any command in this script does succeed.
-#set -e
+set -e
 
 #
 # Lets switch to the /opt/jboss dir
@@ -17,49 +17,53 @@
 cd /opt/jboss
 
 # Download and extract the distro
-#curl -O ${FUSE_DISTRO_URL}
-#jar -xvf ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
-#rm ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
-#mv jboss-fuse-${FUSE_VERSION} jboss-fuse
+curl -O ${FUSE_DISTRO_URL}
+jar -xvf ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
+rm ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
+mv jboss-fuse-${FUSE_VERSION} jboss-fuse
 #ln -s jboss-fuse-${FUSE_VERSION} jboss-fuse
-#chmod a+x jboss-fuse/bin/*
-#rm jboss-fuse/bin/*.bat jboss-fuse/bin/start jboss-fuse/bin/stop jboss-fuse/bin/status jboss-fuse/bin/patch
+chmod a+x jboss-fuse/bin/*
+#rm jboss-fuse/bin/*.bat
+rm jboss-fuse/bin/start jboss-fuse/bin/stop jboss-fuse/bin/status jboss-fuse/bin/patch
 
 # Lets remove some bits of the distro which just add extra weight in a docker image.
-#rm -rf jboss-fuse/extras
-#rm -rf jboss-fuse/quickstarts
+rm -rf jboss-fuse/extras
+rm -rf jboss-fuse/quickstarts
 
 #
 # Let the karaf container name/id come from setting the FUSE_KARAF_NAME && FUSE_RUNTIME_ID env vars
 # default to using the container hostname.
-#sed -i -e 's/environment.prefix=FABRIC8_/environment.prefix=FUSE_/' jboss-fuse/etc/system.properties
-#sed -i -e '/karaf.name = root/d' jboss-fuse/etc/system.properties
-#sed -i -e '/runtime.id=/d' jboss-fuse/etc/system.properties
-#echo '
-#if [ -z "$FUSE_KARAF_NAME" ]; then
-#  export FUSE_KARAF_NAME="$HOSTNAME"
-#fi
-#if [ -z "$FUSE_RUNTIME_ID" ]; then
-#  export FUSE_RUNTIME_ID="$FUSE_KARAF_NAME"
-#fi
-#export KARAF_OPTS="-Dkaraf.name=${FUSE_KARAF_NAME} -Druntime.id=${FUSE_RUNTIME_ID}"
-#'>> jboss-fuse/bin/setenv
+sed -i -e 's/environment.prefix=FABRIC8_/environment.prefix=FUSE_/' jboss-fuse/etc/system.properties
+sed -i -e '/karaf.name = root/d' jboss-fuse/etc/system.properties
+sed -i -e '/runtime.id=/d' jboss-fuse/etc/system.properties
+echo '
+if [ -z "$FUSE_KARAF_NAME" ]; then
+  export FUSE_KARAF_NAME="$HOSTNAME"
+fi
+if [ -z "$FUSE_RUNTIME_ID" ]; then
+  export FUSE_RUNTIME_ID="$FUSE_KARAF_NAME"
+fi
+export KARAF_OPTS="-Dkaraf.name=${FUSE_KARAF_NAME} -Druntime.id=${FUSE_RUNTIME_ID}"
+'>> jboss-fuse/bin/setenv
 
 #
 # Move the bundle cache and tmp directories outside of the data dir so it's not persisted between container runs
 #
-#mv jboss-fuse/data/tmp jboss-fuse/tmp
-#echo '
-#org.osgi.framework.storage=${karaf.base}/tmp/cache
-#'>> jboss-fuse/etc/config.properties
-#sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/karaf
-#sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/fuse
-#sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/client
-#sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/admin
-#sed -i -e 's/${karaf.data}\/generated-bundles/${karaf.base}\/tmp\/generated-bundles/' jboss-fuse/etc/org.apache.felix.fileinstall-deploy.cfg
+mv jboss-fuse/data/tmp jboss-fuse/tmp
+echo '
+org.osgi.framework.storage=${karaf.base}/tmp/cache
+'>> jboss-fuse/etc/config.properties
+
+#Did not migrate these commands. Useful for use in production
+##sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/karaf
+##sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/fuse
+##sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/client
+##sed -i -e 's/-Djava.io.tmpdir="$KARAF_DATA\/tmp"/-Djava.io.tmpdir="$KARAF_BASE\/tmp"/' jboss-fuse/bin/admin
+##sed -i -e 's/${karaf.data}\/generated-bundles/${karaf.base}\/tmp\/generated-bundles/' jboss-fuse/etc/org.apache.felix.fileinstall-deploy.cfg
 
 # lets remove the karaf.delay.console=true to disable the progress bar
-#sed -i -e 's/karaf.delay.console=true/karaf.delay.console=false/' jboss-fuse/etc/config.properties
+# Already default
+sed -i -e 's/karaf.delay.console=true/karaf.delay.console=false/' jboss-fuse/etc/config.properties
 echo '
 # Root logger
 log4j.rootLogger=INFO, stdout, osgi:*VmLogAppender
@@ -76,4 +80,4 @@ bind.address=0.0.0.0
 '>> jboss-fuse/etc/system.properties
 echo '' >> jboss-fuse/etc/users.properties
 
-rm /opt/jboss/install.sh
+#rm /opt/jboss/install.sh
