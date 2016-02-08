d3a21ef310385ade28cad1e485ae0276fd24c712
cyotee <cyotee@gmail.com>
2016-02-07 20:46:43 -0800

    Attempt to address permissions problems during install and build environment limitations

--- install.sh
+++ install.sh
@@ -104,5 +104,5 @@ echo '' >> jboss-fuse/etc/users.properties
 echo "Removing build artifacts."
 echo "Deleting ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip"
 rm -f ${FUSE_ARTIFACT_ID}-${FUSE_VERSION}.zip
-echo "Deleting ./jboss-fuse."
-rm -rf ./jboss-fuse
+##echo "Deleting ./jboss-fuse."
+##rm -rf ./jboss-fuse
