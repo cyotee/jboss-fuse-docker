4208ff085033efef3e669c1bed9cb57fc911b4fe
cyotee <cyotee@gmail.com>
2016-03-14 15:15:23 -0500

    Updated Docker image name

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ echo "Executing install.sh to download an prepare server artifact."
 ./install.sh
 echo "install.sh completed"
 
-DOCKER_REGISTRY=registry.cloudapps-6b83.oslab.opentlc.com
-#DOCKER_REGISTRY=docker.io
+#DOCKER_REGISTRY=registry.cloudapps-6b83.oslab.opentlc.com
+DOCKER_REGISTRY=docker.io
 
-DOCKER_REGISTRY_NAMESPACE=fuse-demo
-#DOCKER_REGISTRY_NAMESPACE=cyotee
+#DOCKER_REGISTRY_NAMESPACE=fuse-demo
+DOCKER_REGISTRY_NAMESPACE=cyotee
 
-DOCKER_IMAGE_NAME=jboss-fuse-docker
+DOCKER_IMAGE_NAME=jboss-fuse
 #DOCKER_IMAGE_VERSION=$FUSE_VERSION
 DOCKER_IMAGE_VERSION=latest
 
